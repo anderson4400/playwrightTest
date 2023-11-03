5c762fcf80f099485699b90856dc09f172ac4bd7
Anderson Medina <anderson_medina@bhd.com.do>
2023-11-03 17:52:36 -0300

    prueba

--- Automatico/test/example.spec.ts
+++ Automatico/test/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import PageLogin from './support/Pom/PageLogin';
 import { PageDinero } from './support/Pom/PageDinero';
 import {PageCerrarSeccion} from './support/Pom/PageCerrarSeccion';
@@ -6,20 +6,17 @@ import {PageCerrarSeccion} from './support/Pom/PageCerrarSeccion';
 const usuarios = "6643908"
 const password = "@And440040"
 
-test.beforeEach(async({page}) =>{
-  const pageLogin = new PageLogin(page);
-  await pageLogin.goto(usuarios,password);
-});
 
-
-test.afterEach(async({page}) =>{
-  await page.close();
-});
 
 test('Prueba', async ({ page }) => {
  const pageDinero = new PageDinero(page);
  const pageCerrarSeccion = new PageCerrarSeccion(page);
+ const pageLogin = new PageLogin(page);
+
+ await pageLogin.goto(usuarios,password);
  await pageDinero.GetDinero();
  await pageCerrarSeccion.cerrarSeccion();
+
+ await page.close();
   
 });
